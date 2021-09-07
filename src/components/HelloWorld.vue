8b6bb06d1e1eeeee841cb5ae8a45cc4832fb94ee
benfangren <760537564@qq.com>
2021-09-07 23:25:05 +0800

    element ui 组件练习

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -43,6 +43,20 @@
         ><div class="grid-content bg-purple-light">4</div></el-col
       >
     </el-row>
+    <h1>布局容器</h1>
+    <el-container>
+      <el-aside width="120px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main <el-aside width="120px">Aside</el-aside></el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+    <h1>图标</h1>
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
   </div>
 </template>
 
@@ -54,6 +68,40 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 .el-row {
   margin-bottom: 20px;
   &:last-child {
